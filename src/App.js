2a8296ccb2f439a7e0729b5e352740ac8c91e156
oguz gül <ogzgl95@outlook.com>
2021-08-06 15:35:33 +0300

    Get all the movies and build row

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Row from "./Row";
+import request from "./request";
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // if [], run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img src={movie.poster_path} alt={movie.name} />
+        ))}
+      </div>
     </div>
   );
 }

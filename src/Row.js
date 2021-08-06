bf52f90ea24b347ebe82cb1b6960d2f4daf96734
oguz gül <ogzgl95@outlook.com>
2021-08-06 16:10:35 +0300

    Design Row component with Row.css

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -20,7 +23,12 @@ function Row({ title, fetchUrl }) {
 
       <div className="row__posters">
         {movies.map((movie) => (
-          <img src={movie.poster_path} alt={movie.name} />
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${baseUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
         ))}
       </div>
     </div>
